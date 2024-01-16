dcda3484152b4d71e2906904002cfa98290f4d97
Richard <moorerichardplease@gmail.com>
2024-01-16 13:10:27 +0900

    Fix box shadows and javascript style for button

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function checkpass() {
     if (document.querySelector('#password').value == 
     document.querySelector('#passconfirm').value) {
         button.disabled = false;
-        button.style.backgroundColor = "rgb(22, 22, 49)";
+        button.style.backgroundColor = "";
     } else {
         passwordMessage.textContent = 'Passwords do not match.';
+        button.disabled = true;
+        button.style.backgroundColor = "gray";
     }
 }
